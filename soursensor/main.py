f06bc871d92947f856a4c601bfd732506f0e3210
Will <william.drazin@gmail.com>
2020-04-16 00:42:57 +0100

    Integrate with InfluxDB

--- soursensor/main.py
+++ soursensor/main.py
@@ -1,10 +1,31 @@
-#!/usr/bin/python3
-
 from sensor.bme680 import BME680
+import datetime
+from influxdb import InfluxDBClient
+from influxdb.client import InfluxDBClientError
+
 
 sensor = BME680()
-print(sensor.get_data())
+data = sensor.get_data()
+print(data)
 
+USER = 'root'
+PASSWORD = 'root'
+DBNAME = 'soursensor'
 
+host = 'localhost'
+port = 8086
 
+point = {
+  "time": datetime.datetime.utcnow(),
+  "measurement": "temperature",
+  "fields": {
+    "value": data['temperature'],
+  },
+  "tags": {
+    "tag": "tag",
+  }
+}
 
+client = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
+
+client.write_points([point])
